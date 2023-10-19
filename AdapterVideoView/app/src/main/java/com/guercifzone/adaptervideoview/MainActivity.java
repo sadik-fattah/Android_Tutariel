beb419318e875f7831fad1ff442ce6e65c742e1a
sadik-fattah <sadikfattah5@gmail.com>
2023-10-19 17:24:54 +0100

    adapter with videoview

---
+++ AdapterVideoView/app/src/main/java/com/guercifzone/adaptervideoview/MainActivity.java
@@ -0,0 +1,110 @@
+package com.guercifzone.adaptervideoview;
+
+import android.media.MediaPlayer;
+import android.view.View;
+import android.widget.*;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.guercifzone.adaptervideoview.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private VideoView video_View;
+    private ListView list_View;
+    private MediaPlayer media_Player;
+    ArrayList<String> video_list;
+    ArrayAdapter adapter;
+    String  video1 ="https://scontent.frba4-3.fna.fbcdn.net/v/t42.1790-2/10000000_338149485427624_3579931041743687779_n.mp4?_nc_cat=102&ccb=1-7&_nc_sid=55d0d3&efg=eyJ2ZW5jb2RlX3RhZyI6InN2ZV9zZCJ9&_nc_eui2=AeGYI56V87cnfXBpWAU5zw66im-CuFKNonaKb4K4Uo2iduGZu_xQc4TlSbw7hh1ObMm59wKXdbD_k-KuJnkJXiMN&_nc_ohc=9fE0O5oqrEIAX_h2zQW&_nc_rml=0&_nc_ht=scontent.frba4-3.fna&oh=00_AfC2h2K6p1oIv0T_RX0L3AGyq5JPD2rjtHJAI6D-p9v4ZA&oe=65367BB7",
+    video2="https://scontent.frba4-1.fna.fbcdn.net/v/t42.1790-2/10000000_1428490621042527_4189696018787484157_n.mp4?_nc_cat=109&ccb=1-7&_nc_sid=55d0d3&efg=eyJ2ZW5jb2RlX3RhZyI6InN2ZV9zZCJ9&_nc_eui2=AeEqO7lZUr_bgb55fM2fEvi9HIcZcDoSJacchxlwOhIlp9Alo-w0WEkWzUo4J5KJwCAUIS95DV0PhgSYEBiCbfIb&_nc_ohc=aBKBIFeJLZwAX_jDaBv&_nc_rml=0&_nc_ht=scontent.frba4-1.fna&oh=00_AfDaibNmrJwVXrz76fqMDH56wQzEQY-QEhihxtD2JIPnOA&oe=653661EC",
+    video3="https://scontent.frba4-1.fna.fbcdn.net/v/t42.1790-2/10000000_895768265494713_4244913410464135917_n.mp4?_nc_cat=105&ccb=1-7&_nc_sid=55d0d3&efg=eyJ2ZW5jb2RlX3RhZyI6InN2ZV9zZCJ9&_nc_eui2=AeEg2fxjNHhugZ8c1qznyuod9ZVMB27lbqf1lUwHbuVup7hDiAZj4nIgTVPfpl7yO-F_G8ZFjEg7Zb7OP6ok69UE&_nc_ohc=YehH_pASh4cAX8FTD3d&_nc_rml=0&_nc_ht=scontent.frba4-1.fna&oh=00_AfAiClraAAon7oe9x_Be0NfBci8NGjDZmtEig2EIz1xNuA&oe=653730EE",
+    video4="https://scontent.frba4-3.fna.fbcdn.net/v/t42.1790-2/10000000_652986416961129_7137215101806612276_n.mp4?_nc_cat=101&ccb=1-7&_nc_sid=55d0d3&efg=eyJ2ZW5jb2RlX3RhZyI6InN2ZV9zZCJ9&_nc_eui2=AeGMwRVEf7r4cDTidsxKB_P6NZLo85VNvyY1kujzlU2_JkOpe0SfrF0g2R2Ee4OkFivCI7W0NT3ojzJqk9XITTTy&_nc_ohc=9lWxVxYPNTcAX9kFaWs&_nc_rml=0&_nc_ht=scontent.frba4-3.fna&oh=00_AfAw7VbRWYQUoeaQH6muaEzuvrHeqBqz5TpUj3kxPrWgwg&oe=6537361A";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list_View = (ListView) findViewById(R.id.listview);
+        video_View = (VideoView) findViewById(R.id.videoview);
+        releaseMedia();
+        video_list= new ArrayList<>();
+        video_list.add("الفيديو  الأول تم حجز متغير");
+        video_list.add("الفيديو الثاني رابط مباشر");
+        video_list.add("الفيديو الثالث تم حجز متغير");
+        video_list.add("الفيديو الرابع رابط مباشر");
+        video_list.add("الفيديو الخامس تم حجز متغير");
+        video_list.add("الفيديو السادس رابط مباشر");
+        video_list.add("الفيديو السابع  تم حجز متغير");
+        video_list.add("الفيديو الثامن رابط مباشر");
+        adapter =  new ArrayAdapter(this, androidx.constraintlayout.widget.R.layout.support_simple_spinner_dropdown_item,video_list);
+        list_View.setAdapter(adapter);
+
+        list_View.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                switch (position) {
+                    case 0:
+
+                        video_View.setVideoPath(video1.toString());
+                        video_View.start();
+                        break;
+                    case 1:
+                        video_View.setVideoPath("https://scontent.frba4-3.fna.fbcdn.net/v/t42.1790-2/10000000_338149485427624_3579931041743687779_n.mp4?_nc_cat=102&ccb=1-7&_nc_sid=55d0d3&efg=eyJ2ZW5jb2RlX3RhZyI6InN2ZV9zZCJ9&_nc_eui2=AeGYI56V87cnfXBpWAU5zw66im-CuFKNonaKb4K4Uo2iduGZu_xQc4TlSbw7hh1ObMm59wKXdbD_k-KuJnkJXiMN&_nc_ohc=9fE0O5oqrEIAX_h2zQW&_nc_rml=0&_nc_ht=scontent.frba4-3.fna&oh=00_AfC2h2K6p1oIv0T_RX0L3AGyq5JPD2rjtHJAI6D-p9v4ZA&oe=65367BB7");
+                        video_View.start();
+                        break;
+                    case 2:
+                        video_View.setVideoPath(video2.toString());
+                        video_View.start();
+                        break;
+                    case 3:
+                        video_View.setVideoPath("https://scontent.frba4-1.fna.fbcdn.net/v/t42.1790-2/10000000_1428490621042527_4189696018787484157_n.mp4?_nc_cat=109&ccb=1-7&_nc_sid=55d0d3&efg=eyJ2ZW5jb2RlX3RhZyI6InN2ZV9zZCJ9&_nc_eui2=AeEqO7lZUr_bgb55fM2fEvi9HIcZcDoSJacchxlwOhIlp9Alo-w0WEkWzUo4J5KJwCAUIS95DV0PhgSYEBiCbfIb&_nc_ohc=aBKBIFeJLZwAX_jDaBv&_nc_rml=0&_nc_ht=scontent.frba4-1.fna&oh=00_AfDaibNmrJwVXrz76fqMDH56wQzEQY-QEhihxtD2JIPnOA&oe=653661EC");
+                        video_View.start();
+                        break;
+                    case 4:
+                        video_View.setVideoPath(video3.toString());
+                        break;
+                    case 5:
+                        video_View.setVideoPath("https://scontent.frba4-1.fna.fbcdn.net/v/t42.1790-2/10000000_895768265494713_4244913410464135917_n.mp4?_nc_cat=105&ccb=1-7&_nc_sid=55d0d3&efg=eyJ2ZW5jb2RlX3RhZyI6InN2ZV9zZCJ9&_nc_eui2=AeEg2fxjNHhugZ8c1qznyuod9ZVMB27lbqf1lUwHbuVup7hDiAZj4nIgTVPfpl7yO-F_G8ZFjEg7Zb7OP6ok69UE&_nc_ohc=YehH_pASh4cAX8FTD3d&_nc_rml=0&_nc_ht=scontent.frba4-1.fna&oh=00_AfAiClraAAon7oe9x_Be0NfBci8NGjDZmtEig2EIz1xNuA&oe=653730EE");
+                        video_View.start();
+                        break;
+                    case 6:
+                        video_View.setVideoPath(video4.toString());
+                        video_View.start();
+                        break;
+                    case 7:
+                        video_View.setVideoPath("https://scontent.frba4-3.fna.fbcdn.net/v/t42.1790-2/10000000_652986416961129_7137215101806612276_n.mp4?_nc_cat=101&ccb=1-7&_nc_sid=55d0d3&efg=eyJ2ZW5jb2RlX3RhZyI6InN2ZV9zZCJ9&_nc_eui2=AeGMwRVEf7r4cDTidsxKB_P6NZLo85VNvyY1kujzlU2_JkOpe0SfrF0g2R2Ee4OkFivCI7W0NT3ojzJqk9XITTTy&_nc_ohc=9lWxVxYPNTcAX9kFaWs&_nc_rml=0&_nc_ht=scontent.frba4-3.fna&oh=00_AfAw7VbRWYQUoeaQH6muaEzuvrHeqBqz5TpUj3kxPrWgwg&oe=6537361A");
+                        video_View.start();
+                        break;
+                }
+                video_View.setMediaController(new MediaController(MainActivity.this));
+                video_View.requestFocus();
+
+            }
+        });
+    }
+
+    private void releaseMedia() {
+
+        if (media_Player != null){
+            media_Player.release();
+            media_Player =null;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        video_View.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        video_View.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMedia();
+    }
+}
